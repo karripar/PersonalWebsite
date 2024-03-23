88d76362ce88ced2c8ac48ba4491db3334221c87
karripartanen <karripartanen25@gmail.com>
2024-03-23 22:18:56 +0200

    Initial commit 23.3.2024

---
+++ CSS1/script.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const rat_image = document.getElementById("ChaseTheRat");
+
+rat_image.addEventListener('click', function() {
+    window.location.href = 'https://github.com/lummila/projekti-2';
+})
