942009a3fde39267db5201645594f39882d44372
karripar <karripar@metropolia.fi>
2024-06-11 22:54:15 +0300

    Several modifications and additions, animations

--- CSS1/script.js
+++ CSS1/script.js
@@ -4,10 +4,30 @@ const ratImage = document.getElementById("ChaseTheRat");
 
 ratImage.addEventListener('click', function() {
     window.location.href = 'https://github.com/lummila/projekti-2';
-})
+});
 
 const htmlImage = document.getElementById("htmlRepo");
 
 htmlImage.addEventListener('click', function() {
     window.location.href = 'https://github.com/karripartanen/PersonalWebsite';
-})
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('.product-container section');
+    sections.forEach((section, index) => {
+        setTimeout(() => {
+            section.classList.add('show')
+        }, index * 500);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
