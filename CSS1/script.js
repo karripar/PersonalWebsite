62cc78e6875f67dc9f01d5e50272c11dd7281e36
karripar <karripar@metropolia.fi>
2024-06-18 23:23:50 +0300

    Visual bug fixes and animation tweaks

--- CSS1/script.js
+++ CSS1/script.js
@@ -1,17 +1,5 @@
 'use strict'
 
-const ratImage = document.getElementById("ChaseTheRat");
-
-ratImage.addEventListener('click', function() {
-    window.location.href = 'https://github.com/lummila/projekti-2';
-});
-
-const htmlImage = document.getElementById("htmlRepo");
-
-htmlImage.addEventListener('click', function() {
-    window.location.href = 'https://github.com/karripar/PersonalWebsite';
-});
-
 document.addEventListener('DOMContentLoaded', function() {
     const sections = document.querySelectorAll('.product-container section');
     sections.forEach((section, index) => {
@@ -22,6 +10,50 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+
+// toggles a menu bar on the left edge of the html (button)
+document.getElementById('menu-toggle').addEventListener('click', function(event) {
+    event.stopPropagation();
+    const menu = document.querySelector('.dropdown_menu');
+    const toggleButton = document.getElementById('menu-toggle');
+    console.log('Current width:', menu.style.width);
+    if (menu.style.width === '250px') {
+        menu.style.width = '0';
+        toggleButton.innerHTML = '&#9654;'; // arrow to the right
+        toggleButton.style.left = '0'; // button moves to the left
+    } else {
+        menu.style.width = '250px';
+        toggleButton.innerHTML = '&#9664;'; // arrow to the left
+        toggleButton.style.left = '250px'; // button moves to the right
+    }
+});
+
+document.addEventListener('click', function() {
+    const menu = document.querySelector('.dropdown_menu');
+    const toggleButton = document.getElementById('menu-toggle');
+    if (menu.style.width === '250px') {
+        menu.style.width = '0';
+        toggleButton.innerHTML = '&#9654;'; // arrow to the right
+        toggleButton.style.left = '0'; // button moves to the left
+    }
+});
+
+document.querySelector('.dropdown_menu').addEventListener('click', function(event) {
+    event.stopPropagation();
+});
+
+
+
+const ratImage = document.getElementById("ChaseTheRat");
+ratImage.addEventListener('click', function() {
+    window.location.href = 'https://github.com/lummila/projekti-2';
+});
+
+
+const htmlImage = document.getElementById("htmlRepo");
+htmlImage.addEventListener('click', function() {
+    window.location.href = 'https://github.com/karripar/PersonalWebsite';
+});
 
 
 
